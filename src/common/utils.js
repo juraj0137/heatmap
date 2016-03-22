7b667422a7dcf92fc75610e511ac9aa4ffb9e2c6
Juraj Kubala <juraj.alabuk@gmail.com>
2016-03-22 07:55:52 +0100

    utilities + model

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,11 +1,11 @@
-export class HeatmapUtils {
+class HeatmapUtils {
 
     /**
      * Returns center position of element
      * @param elem
      * @returns {{x: number, y: number}}
      */
-    static getElementPosition(elem) {
+    static getElementCenterPosition(elem) {
 
         // (1)
         var box = elem.getBoundingClientRect();
@@ -25,6 +25,83 @@ export class HeatmapUtils {
         var left = ((box.left + scrollLeft - clientLeft) + (box.width / 2));
 
         return {x: Math.round(left), y: Math.round(top)};
+    }
+
+    /**
+     * Returns attention area of element
+     * Rozdeli element na siet, 10x10 a vrati hodnotu podla toho, v ktorej casti elementu sa kurzor nachadza
+     * @param event
+     * @returns {{x: number, y: number}}
+     */
+    static getElementAttentionArea(event) {
+
+        //console.log(elem);
+
+        let pageX = event.pageX;
+        let pageY = event.pageY;
+
+        // (1)
+        var box = event.target.getBoundingClientRect();
+        var body = document.body;
+        var docElem = document.documentElement;
+
+        // (2)
+        var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
+        var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft;
+
+        // (3)
+        var clientTop = docElem.clientTop || body.clientTop || 0;
+        var clientLeft = docElem.clientLeft || body.clientLeft || 0;
+
+        // (4)
+        var elmX = pageX - (box.left + scrollLeft - clientLeft);
+        var elmY = pageY - (box.top + scrollTop - clientTop);
+
+        var x = Math.floor((elmX * 10 ) / box.width);
+        var y = Math.floor((elmY * 10 ) / box.height);
+
+        return {x: x, y: y};
+    }
+
+    static getElementAttentionAreaPosition(element, x, y) {
+
+        let box = element.getBoundingClientRect();
+        let centerOfElement = this.getElementCenterPosition(element);
+
+        let top = centerOfElement.y - (box.height / 2);
+        let left = centerOfElement.x - (box.width / 2);
+
+        return {
+            x: Math.round(left + ((box.width / 10) * x ) + (box.width / 20)),
+            y: Math.round(top + ((box.height / 10) * y ) + (box.height / 20))
+        };
+
+    }
+
+    static getEventPath(event) {
+        var path = [];
+        var node = event.target;
+        while (node != undefined) {
+            path.push(node);
+            node = node.parentNode;
+        }
+        if (path.length > 0) {
+            path.push(window);
+        }
+        return path;
+    }
+
+    static getPositionBetweenSiblings(element) {
+        var index = -1;
+        if (element.parentElement != null) {
+            for (let i = 0; i < element.parentElement.children.length; i++) {
+                if (element.parentElement.children[i] === element) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        return index;
     }
 
     /**
@@ -82,4 +159,14 @@ export class HeatmapUtils {
         return bytes;
     }
 
+    static uuid() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+
 }
+
+export {HeatmapUtils};
