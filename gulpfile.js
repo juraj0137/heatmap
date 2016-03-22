a118d82ca33e0eb2820d42d245e03483273f15d9
Juraj Kubala <juraj.alabuk@gmail.com>
2016-03-22 07:35:35 +0100

    uprava buildovacich configov + gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,13 @@
 var production = false;
 var gulp = require('gulp');
-var path = require('path');
-//var webpack = require('gulp-webpack');
 var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var GulpUtil = require('gulp-util');
-var syncfolder = '';
+var config = require('./webpack.config');
 
 gulp.task('build', function (callback) {
     console.log('Ecosystem is production', production);
 
-    var plugins = [
-        new ExtractTextPlugin("bundle.css", {allChunks: true})
-    ];
-
-    var webpackConfig = {
-        entry: {
-            client: './src/client/index.js',
-            admin: './src/admin/index.js',
-            server: './src/server/index.js',
-        },
-        output: {
-            path: path.join(__dirname, "build"),
-            filename: "[name]/bundle.js"
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.jsx?$/,
-                    loader: 'babel?presets[]=react&presets[]=es2015',
-                    exclude: /node_modules/
-                },
-                {
-                    test: /\.styl$/,
-                    loader: ExtractTextPlugin.extract("stylus", "css-loader!stylus-loader"),
-                    exclude: /node_modules/
-                },
-                {
-                    test: /\.css/,
-                    loader: ExtractTextPlugin.extract("css-loader"),
-                    exclude: /node_modules/
-                }
-            ]
-        },
-        plugins: plugins
-    };
-
-    if (!production) {
-        webpackConfig.devtool = 'inline-source-map';
-    } else {
-        plugins.push(new webpackOrig.optimize.UglifyJsPlugin({minimize: true}));
-    }
-
-    webpack(webpackConfig, function(err, stats) {
+    webpack(config(production), function(err, stats) {
         if(err) throw new GulpUtil.PluginError("webpack:build", err);
         GulpUtil.log("[webpack:build]", stats.toString({
             colors: true
