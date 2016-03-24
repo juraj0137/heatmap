cbc9f38e741b1a3d690d311f00ea864022a0299d
Juraj Kubala <juraj.alabuk@gmail.com>
2016-03-24 18:16:08 +0100

    api na visit data

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -2,13 +2,6 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
 
-//import {deleteIndex,initIndex, initMapping} from './../elastic/heatmaps.js';
-//deleteIndex().then(()=>{
-//    initIndex().then(()=>{
-//        initMapping();
-//    });
-//});
-
 var app = express();
 
 app.use(cors());
@@ -16,5 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/heatmaps', require('./heatmaps.js'));
+app.use('/collect', require('./collectData.js'));
 
 module.exports = app;
