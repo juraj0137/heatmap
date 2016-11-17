0f5996ee42968779a7b6cf8817cb243a77576642
Juraj Kubala <juraj.alabuk@gmail.com>
2016-11-17 12:11:16 +0200

    changed IP

--- src/client/config/config.js
+++ src/client/config/config.js
@@ -4,7 +4,7 @@
 const configHeatmap = {
     enable: true, // enable tracking
     population: 100, // percent
-    collectUrl: "http://128.199.44.148/api/visit",
+    collectUrl: "http://188.166.29.100/api/visit",
     collectInterval: 60, //ms
     idleTime: 1000 //ms
 };
